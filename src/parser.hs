dcb7803174d35e084f7db063cb4f98f01eb9a92d
Jin SANO <sano-jin@akane.waseda.jp>
2020-09-07 22:30:16 +0900

    updated

--- src/parser.hs
+++ src/parser.hs
@@ -5,7 +5,13 @@ module Parser (
   Proc (FromLocal, FromFreeTail, Rule)
   ) where
 import Data.List
+import Control.Monad
 import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Language
+import Text.ParserCombinators.Parsec.Token as Token
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
 uncurry3 f (a, b, c) = f a b c 
@@ -16,47 +22,47 @@ data Pointer = Pointer String
 data Proc = FromLocal (Maybe String) String [Pointer]
           | FromFreeTail String String [Pointer]
           | Alias String String
-          | Rule [Proc] [Proc]
+          | Rule (S.Set Proc) (S.Set Proc)
 
--- show
-showBlock :: [Proc] -> String
-showBlock = intercalate ". " . map showProc
+languageDef =
+  emptyDef { Token.commentStart    = "/*"
+           , Token.commentStart    = "*/"
+           , Token.commentStart    = "%"
+           , Token.identStart      = letter
+           , Token.identLetter     = alphaNum <|> char '_'
+           , Token.reservedNames   = []
+           , Token.reservedOpNames = []
+           }                                     
 
-showProcList :: [Proc] -> String
-showProcList = intercalate ", " . map showProc_
-  where showProc_ r@(Rule _ _) = "(" ++ showProc r ++ ")"
-        showProc_ otherwise = showProc otherwise
+lexer = Token.makeTokenParser languageDef
 
-showPointerList :: [Pointer] -> String
-showPointerList [] = ""
-showPointerList args = "(" ++ unwordsList args ++ ")"
-  where unwordsList = intercalate ", " . map showPointer
+atomName =
+  Token.lexeme $ try $ do c <- lower
+                          cs <- many (alphaNum <|> char '_')
+                          return $ c : cs
+pointerName = 
+  Token.lexeme $ try $ do c <- (upper <|> '_')
+                          cs <- many (alphaNum <|> char '_')
+                          return $ c : cs
 
-showPointer :: Pointer -> String
-showPointer (Pointer pointer) = pointer
-showPointer (Atom name args) = name ++ showPointerList args
-
-showProc :: Proc -> String
-showProc (FromLocal (Just parent) name args) = parent ++ " -> " ++ name ++ showPointerList args
-showProc (FromLocal Nothing name args) = name ++ showPointerList args
-showProc (FromFreeTail parent name args) = "*" ++ parent ++ " -> " ++ name ++ showPointerList args
-showProc (Rule lhs rhs) = showProcList lhs ++ " :- " ++ showProcList rhs
+parens      = Token.parens     lexer
+reserevedOP = Token.reservedOp lexer
+comma       = Token.comma      lexer
+dot         = Token.dot        lexer 
+whiteSpace  = Token.whiteSpace lexer 
+arrow       = Token.lexeme $ string "->"
 
 -- parser
-pointerName :: Parser String
-pointerName = do x <- upper <|> char '_'
-                 y <- many (alphaNum <|> char '_')
-                 return $ x : y 
-
-atomName :: Parser String
-atomName = (do x <- lower
-               y <- many (alphaNum <|> char '_')
-               return $ x : y)
-           <|> do char '`'
-                  x <- many $ noneOf "`"
-                  char '`'
-                  return x
-
+whileParser :: Parser (S.Set Proc)
+whileParser = liftM (S.fromList) $ whiteSpace >> parseBlock
+  
+parseBlock :: Parser [Proc]
+parseBlock = sepEndBy1 parseLine dot
+             
+parseLine :: Parser [Proc]
+parseLine = do x <- parseList
+               do char 
+                    
 parsePointer :: Parser Pointer
 parsePointer = (return . Pointer =<< pointerName)
                <|> return . uncurry Atom =<< parseAtomBody
@@ -119,3 +125,30 @@ parseBlock = do spaces
 
 readExpr :: String -> Either ParseError [Proc]
 readExpr = parse parseBlock "vertex"
+
+
+
+
+-- show
+showBlock :: S.Set Proc -> String
+showBlock = intercalate ". " . map showProc . S.toList
+
+showProcList :: S.Set Proc -> String
+showProcList = intercalate ", " . map showProc_ . S.toList
+  where showProc_ r@(Rule _ _) = "(" ++ showProc r ++ ")"
+        showProc_ others = showProc others
+
+showPointerList :: [Pointer] -> String
+showPointerList [] = ""
+showPointerList args = "(" ++ unwordsList args ++ ")"
+  where unwordsList = intercalate ", " . map showPointer
+
+showPointer :: Pointer -> String
+showPointer (Pointer pointer) = pointer
+showPointer (Atom name args) = name ++ showPointerList args
+
+showProc :: Proc -> String
+showProc (FromLocal (Just parent) name args) = parent ++ " -> " ++ name ++ showPointerList args
+showProc (FromLocal Nothing name args) = name ++ showPointerList args
+showProc (FromFreeTail parent name args) = "*" ++ parent ++ " -> " ++ name ++ showPointerList args
+showProc (Rule lhs rhs) = showProcList lhs ++ " :- " ++ showProcList rhs
