ff5a8327037156103f7d9d8362f33e9ee50666c0
Jin SANO <sano-jin@akane.waseda.jp>
2020-11-27 22:19:57 +0900

    applied brittany

--- src/Util/Util.hs
+++ src/Util/Util.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE Safe #-}
-module Util.Util (
-  mapEitherList,
-  monadicMapAccumL,
-  monadicFoldl
+module Util.Util
+  ( mapEitherList
+  , monadicMapAccumL
+  , monadicFoldl
   ) where
 import           Control.Monad.Except
 
@@ -17,9 +17,8 @@ mapEitherList :: (a -> Either b c) -> [a] -> Either b [c]
 mapEitherList = monadicMapList
 
 monadicMapList :: Monad m => (a -> m c) -> [a] -> m [c]
-monadicMapList f (h:t)
-  = liftM2 (:) (f h) $ monadicMapList f t
-monadicMapList _ [] = return []
+monadicMapList f (h : t) = liftM2 (:) (f h) $ monadicMapList f t
+monadicMapList _ []      = return []
 
 {--|
 mapMaybeList :: (a -> Maybe b) -> [a] -> Maybe [b]
@@ -30,13 +29,12 @@ mapMaybeList _ [] = return []
 
 -- | A monadic version of List.mapAccumL
 monadicMapAccumL :: Monad m => (a -> b -> m (a, c)) -> a -> [b] -> m (a, [c])
-monadicMapAccumL f a (b:bs) =
-  do (a', c) <- f a b
-     (a'', cs) <- monadicMapAccumL f a' bs
-     return (a'', c:cs)
+monadicMapAccumL f a (b : bs) = do
+  (a' , c ) <- f a b
+  (a'', cs) <- monadicMapAccumL f a' bs
+  return (a'', c : cs)
 monadicMapAccumL _ a [] = return (a, [])
 
 monadicFoldl :: Monad m => (acc -> b -> m acc) -> acc -> [b] -> m acc
-monadicFoldl f acc (h:t)
-  = flip (monadicFoldl f) t =<< f acc h
-monadicFoldl _ acc [] = return acc
+monadicFoldl f acc (h : t) = flip (monadicFoldl f) t =<< f acc h
+monadicFoldl _ acc []      = return acc
