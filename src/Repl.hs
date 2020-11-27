b5c2445d6c72b2e4fde2924431147cd7ac45b217
Jin SANO <sano-jin@akane.waseda.jp>
2020-11-27 22:00:17 +0900

    applied brittany

--- src/Repl.hs
+++ src/Repl.hs
@@ -1,13 +1,17 @@
 module Repl where
 -- import System.Environment
-import           Compiler.Compiler  (compile)
-import           Compiler.Normalize (normalize)
-import           Compiler.Process   (Rule)
+import           Compiler.Compiler              ( compile )
+import           Compiler.Normalize             ( normalize )
+import           Compiler.Process               ( Rule )
 import           Data.List
 import           Data.Maybe
 import           System.IO
-import           VM.VM              (State (..), initializeHeap, isStateEq,
-                                     reduce, reduceND)
+import           VM.VM                          ( State(..)
+                                                , initializeHeap
+                                                , isStateEq
+                                                , reduce
+                                                , reduceND
+                                                )
 -- import Util.Util
 import           Control.Monad
 import           Data.Tuple.Extra
@@ -18,35 +22,37 @@ flushStr :: String -> IO ()
 flushStr str = putStr str >> hFlush stdout
 
 run :: (State -> String) -> Int -> State -> IO ()
-run state2String stepN oldState
-  = case reduce oldState of
-      Just (newState, rule)
-        -> putStrLn
-           (show stepN ++ ": "
-             ++ show rule ++ " ~> \n"
-             ++ state2String newState ++ "\n")
-           >> run state2String (stepN + 1) newState
-      Nothing -> return ()
+run state2String stepN oldState = case reduce oldState of
+  Just (newState, rule) ->
+    putStrLn
+        (  show stepN
+        ++ ": "
+        ++ show rule
+        ++ " ~> \n"
+        ++ state2String newState
+        ++ "\n"
+        )
+      >> run state2String (stepN + 1) newState
+  Nothing -> return ()
 
-readAndRun:: (State -> String) -> String -> IO ()
-readAndRun state2String input
-  = case normalize =<< compile input of
-    Left err -> putStrLn ("Error : " ++ show err)
-    Right (procVals, rules) ->
-      let initialState = State (initializeHeap procVals) rules in
-        putStrLn ("0: \n" ++ state2String initialState ++ "\n")
-        >> run state2String 1 initialState
+readAndRun :: (State -> String) -> String -> IO ()
+readAndRun state2String input = case normalize =<< compile input of
+  Left err -> putStrLn ("Error : " ++ show err)
+  Right (procVals, rules) ->
+    let initialState = State (initializeHeap procVals) rules
+    in  putStrLn ("0: \n" ++ state2String initialState ++ "\n")
+          >> run state2String 1 initialState
 
 
-readAndRunND:: (State -> String) -> String -> IO ()
-readAndRunND state2String input
-  = case normalize =<< compile input of
-    Left err -> putStrLn ("Error : " ++ show err)
-    Right (procVals, rules) ->
-      let initialState = State (initializeHeap procVals) rules in
-        putStrLn ("0: " ++ state2String initialState)
-        >> runND state2String 0 (initialPath initialState) initialState
-        >>= putStrLn . showEnds state2String
+readAndRunND :: (State -> String) -> String -> IO ()
+readAndRunND state2String input = case normalize =<< compile input of
+  Left err -> putStrLn ("Error : " ++ show err)
+  Right (procVals, rules) ->
+    let initialState = State (initializeHeap procVals) rules
+    in  putStrLn ("0: " ++ state2String initialState)
+          >>  runND state2String 0 (initialPath initialState) initialState
+          >>= putStrLn
+          .   showEnds state2String
 
 
 data Path = Path Int [(Int, State)] [((Int, Int), Rule)]
@@ -63,21 +69,22 @@ initialPath :: State -> Path
 initialPath state = Path 1 [(0, state)] []
 
 
-addState2Path :: Int -> Path -> (State, Rule) -> (Path, Maybe (Int, (State, Rule)))
+addState2Path
+  :: Int -> Path -> (State, Rule) -> (Path, Maybe (Int, (State, Rule)))
 addState2Path oldStateId (Path stateN states transitions) (newState, appliedRule)
-  = case find (isStateEq newState. snd) states of
-      Nothing
-        -> ( Path
-             (stateN + 1)
+  = case find (isStateEq newState . snd) states of
+    Nothing ->
+      ( Path (stateN + 1)
              ((stateN, newState) : states)
              (((oldStateId, stateN), appliedRule) : transitions)
-           , Just (stateN, (newState, appliedRule)))
-      Just (matchedStateId, _)
-        -> ( Path
-             stateN
+      , Just (stateN, (newState, appliedRule))
+      )
+    Just (matchedStateId, _) ->
+      ( Path stateN
              states
              (((oldStateId, matchedStateId), appliedRule) : transitions)
-           , Nothing)
+      , Nothing
+      )
 
 {--|
 printTransitionsND :: (State -> String) -> Int -> (State, Rule) -> IO ()
@@ -89,38 +96,51 @@ printTransitionsND state2String stateN (newState, rule)
 |--}
 
 showEnds :: (State -> String) -> Path -> String
-showEnds state2String (Path stateN states transitions)
-  = "\nEnded with state number: " ++ show stateN ++ ".\n"
+showEnds state2String (Path stateN states transitions) =
+  "\nEnded with state number: "
+    ++ show stateN
+    ++ ".\n"
     ++ "\nstates:\n"
-    ++ unlines (reverse $ map (\(stateId, state)
-                              -> show stateId ++ ": " ++ state2String state) states)
-    ++ "\ntransitions: " ++ show (length transitions) ++ "\n"
-    ++ unlines ((reverse
-                 . map (\((prev, next), rule)
-                         -> show prev ++ " ~> " ++ show next
-                            ++ " with a rule \"" ++ show rule ++ "\"."
-                       )
-                ) transitions)
+    ++ unlines
+         (reverse $ map
+           (\(stateId, state) -> show stateId ++ ": " ++ state2String state)
+           states
+         )
+    ++ "\ntransitions: "
+    ++ show (length transitions)
+    ++ "\n"
+    ++ unlines
+         ((reverse . map
+            (\((prev, next), rule) ->
+              show prev
+                ++ " ~> "
+                ++ show next
+                ++ " with a rule \""
+                ++ show rule
+                ++ "\"."
+            )
+          )
+           transitions
+         )
 
 
 runND :: (State -> String) -> Int -> Path -> State -> IO Path
-runND state2String oldStateID oldPath oldState
-  = let (path, transitions)
-          = second catMaybes
-          $ mapAccumL
-          (addState2Path oldStateID)
-          oldPath
-          (reduceND oldState)
-    in
-      mapM_ (putStrLn . (\(stateId, (state, _))
-                          -> show stateId ++ ": " ++ state2String state)) transitions
-      >> if null transitions
-         then return path
-         else
-           let runND' path (oldStateId, (state, _))
-                 = runND state2String oldStateId path state
-           in
-             foldM runND' path transitions
+runND state2String oldStateID oldPath oldState =
+  let (path, transitions) = second catMaybes
+        $ mapAccumL (addState2Path oldStateID) oldPath (reduceND oldState)
+  in  mapM_
+          ( putStrLn
+          . (\(stateId, (state, _)) ->
+              show stateId ++ ": " ++ state2String state
+            )
+          )
+          transitions
+        >> if null transitions
+             then return path
+             else
+               let runND' path (oldStateId, (state, _)) =
+                     runND state2String oldStateId path state
+               in  foldM runND' path transitions
 
 
 
